aa309f8fcc04082c1a5b117329e5aaa50987e2ab
Brandyn Call <blind_crisis71@hotmail.com>
2019-05-29 18:16:11 -0600

    Testing I can push to the repo

---
+++ helloWorld.php
@@ -0,0 +1,8 @@
+<?php
+
+	$servername = "sql304.epizy.com";
+	$username = "epiz_23868834";
+	$password = "J8qvmlqxA";
+	$dbname = "epiz_23868834_summer3750peterson";
+	
+	echo "Hello world! -Brandyn";
