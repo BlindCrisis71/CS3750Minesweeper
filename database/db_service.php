1ea5ef4e51afe5a5b3ec608d24115ae20f1580ec
Francisco Morales <franciscomorales@mail.weber.edu>
2019-06-10 20:57:33 -0600

    DB Service Changes
    wip

--- database/db_service.php
+++ database/db_service.php
@@ -2,38 +2,50 @@
 /**
  * Database Service
  */
- include('./db_config.php');
- echo("Database Service<br>");
- $Func = new DBService();
- $Func->connectToDB();
+
+ $servername = ftpupload.net;
+ $username = epiz_23946308;
+ $password = FUs75Vs41;
+ $dbname = epiz_23946308_minesweeper;
+
+ // Create connection
+ $conn = new mysqli($servername, $username, $password, $dbname);
+ // Check connection
+ echo("Connecting to DB-Minesweeper<br><br>");
+ if ($conn) {
+     die("Connection failed: " . $conn->connect_error);
+ }
+
+ writeToGameTableDB("1", "Test");
+ $board = getFromGameTableDB("1");
+ echo $board;
 
  class DBService
  {
-   function connectToDB()
+   // Use to write GameId, BoardData to GameTable
+   // Parameter 1: GameId, Parameter 2: BoardData
+   function writeToGameTableDB($gameId, $boardData)
    {
-     $Func = new DBConfig();
-     $Func->initializeDbConnection();
+     $sql = "INSERT INTO GameTable (GameId, BoardData) VALUES ('" . $gameId . "', '" . $boardData . "')";
+     if ($conn->query($sql) === TRUE) {
+       echo "Success"
+     } else {
+       echo "Error: " . $sql . "<br>" . $conn->error;
+     }
    }
 
-   // function writeToDB(table, column, value)
-   // {
-   //   // $sql = "INSERT INTO " . $table . " (" . $username . ") VALUES ('" . $value . "')";
-   //   // if ($conn->query($sql) === TRUE) {
-   //   // } else {
-   //   //   echo "Error: " . $sql . "<br>" . $conn->error;
-   //   // }
-   // }
-   //
-   // function getFromDB(select, table, column, value)
-   // {
-   //   // $sql = "SELECT $select FROM $table WHERE $column = $value";
-   //   // $result = $conn->query($sql);
-   //   // if ($result->num_rows > 0) {
-   //   //    echo "GET DB - SUCCESS<br><br>";
-   //   //  } else {
-   //   //    echo "GET DB - FAILURE<br><br>";
-   //   //  }
-   //   //  return $result
-   //  }
+   // Use to get BoardData from GameTable
+   // Parameter 1: GameId
+   // Return: BoardData
+   function getFromGameTableDB($gameId)
+   {
+     $sql = "SELECT $select FROM GameTable WHERE GameId = $gameId";
+     $result = $conn->query($sql);
+     if ($result->num_rows > 0) {
+        return $result
+      } else {
+        return "Error - DB could not be reached";
+      }
+    }
  }
 ?>
